a8e09d5f85b9092cb3bfe013b5e642d287c027f4
Rivka Weingarten <rw240brooklyn@gmail.com>
2024-02-01 17:09:23 -0500

    removed app.get running on 5000

--- backend/server.js
+++ backend/server.js
@@ -18,9 +18,7 @@ const app = express();
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use(cookieParser())
-app.get('/', (req, res) => {
-res.send("server is on 5000")
-})
+
 
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
