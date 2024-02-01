060fc0174eecfab3c54cb10237cd3fc0971d2d9f
Rivka Weingarten <rw240brooklyn@gmail.com>
2024-02-01 16:53:19 -0500

    removed webhook dependency

--- backend/routes/clerkWebhooks.js
+++ backend/routes/clerkWebhooks.js
@@ -1,6 +1,7 @@
 import express from "express";
 import bodyParser from "body-parser";
-import { Webhook, WebhookEvent } from "@clerk/clerk-sdk-node";
+// import { Webhook } from '@clerk/clerk-sdk-node';
+// import clerkClient from '@clerk/clerk-sdk-node';
 import { deleteUser, registerUser } from "../controllers/userController.js";
 
 const router = express.Router();
